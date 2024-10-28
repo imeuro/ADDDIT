ecb26fc512a3af8a142dab56c1106346bdf114ba
imeuro <mauro.fioravanzi@gmail.com>
2024-10-28 15:50:59 +0100

    sync with en

--- assets/inc/en/values-home.inc.php
+++ assets/inc/en/values-home.inc.php
@@ -1,25 +1,26 @@
 <div class="values-cont flex text">
-	<h3>
+	<!--h3>
 		We're your trusted partner in bringing your ideas to life. Our mission is to simplify the 3D printing process, making it accessible and efficient for businesses and individuals alike.
-	</h3>
+	</h3-->
 
-	<div class="values-pic">
-		<img src="<?php echo $basepath .'/assets/img/1727336307.jpg'; ?>" alt="Mission" width="400" height="714" loading="lazy" />
-	</div>
-	<div class="values-text">
-		<ul>
-			<li>
-				<h4>Accessibility:</h4>
-				<p>We make 3D printing accessible to everyone by streamlining the process from design to delivery. Our platform connects you with the right partners, ensuring a seamless experience.</p>
-			</li>
-			<li>
-				<h4>Efficiency:</h4> 
-				<p>We optimize the 3D printing journey by providing efficient solutions. Our streamlined platform and dedicated support team help you save time and resources.</p>
-			</li>
-			<li>
-				<h4>Reliability:</h4> 
-				<p>You can trust us to deliver on our promises. We partner with reputable 3D printing providers and maintain high standards for quality and customer satisfaction.</p>
-			</li>
-		</ul>
-	</div>
+	<ul class="points-text flex">
+		<li class="flex">
+			<span class="points-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M290.8 48.6l78.4 29.7L288 109.5 206.8 78.3l78.4-29.7c1.8-.7 3.8-.7 5.7 0zM136 92.5l0 112.2c-1.3 .4-2.6 .8-3.9 1.3l-96 36.4C14.4 250.6 0 271.5 0 294.7L0 413.9c0 22.2 13.1 42.3 33.5 51.3l96 42.2c14.4 6.3 30.7 6.3 45.1 0L288 457.5l113.5 49.9c14.4 6.3 30.7 6.3 45.1 0l96-42.2c20.3-8.9 33.5-29.1 33.5-51.3l0-119.1c0-23.3-14.4-44.1-36.1-52.4l-96-36.4c-1.3-.5-2.6-.9-3.9-1.3l0-112.2c0-23.3-14.4-44.1-36.1-52.4l-96-36.4c-12.8-4.8-26.9-4.8-39.7 0l-96 36.4C150.4 48.4 136 69.3 136 92.5zM392 210.6l-82.4 31.2 0-89.2L392 121l0 89.6zM154.8 250.9l78.4 29.7L152 311.7 70.8 280.6l78.4-29.7c1.8-.7 3.8-.7 5.7 0zm18.8 204.4l0-100.5L256 323.2l0 95.9-82.4 36.2zM421.2 250.9c1.8-.7 3.8-.7 5.7 0l78.4 29.7L424 311.7l-81.2-31.1 78.4-29.7zM523.2 421.2l-77.6 34.1 0-100.5L528 323.2l0 90.7c0 3.2-1.9 6-4.8 7.3z"/></svg>
+			</span>
+			<h4>80+ materials,<br>finishes, and <br>post-processing treatments</h4> 
+		</li>
+		<li class="flex">
+			<span class="points-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288l111.5 0L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7l-111.5 0L349.4 44.6z"/></svg>
+			</span>
+			<h4>Get a fast, tailored quote</h4> 
+		</li>
+		<li class="flex">
+			<span class="points-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 512C86 512 0 426 0 320C0 228.8 130.2 57.7 166.6 11.7C172.6 4.2 181.5 0 191.1 0l1.8 0c9.6 0 18.5 4.2 24.5 11.7C253.8 57.7 384 228.8 384 320c0 106-86 192-192 192zM96 336c0-8.8-7.2-16-16-16s-16 7.2-16 16c0 61.9 50.1 112 112 112c8.8 0 16-7.2 16-16s-7.2-16-16-16c-44.2 0-80-35.8-80-80z"/></svg>
+			</span>
+			<h4>24/7 3D printing service,<br>ready to go!</h4> 
+		</li>
+	</ul>
 </div>
