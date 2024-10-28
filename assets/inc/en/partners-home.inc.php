96d603131374cc4e58a331544511309ea5cdc072
imeuro <mauro.fioravanzi@gmail.com>
2024-10-28 15:59:24 +0100

    partners-home

--- assets/inc/en/partners-home.inc.php
+++ assets/inc/en/partners-home.inc.php
@@ -6,5 +6,5 @@
 	<div class="partner-slide">
 		<img src="<?php // echo $basepath .'/assets/img/wasp-logo.png'; ?>" width="300" height="105" alt="Wasp Logo" />
 	</div>
-	
+	-->
 </div>
--- assets/inc/partners-home.inc.php
+++ assets/inc/partners-home.inc.php
@@ -6,52 +6,5 @@
 	<div class="partner-slide">
 		<img src="<?php // echo $basepath .'/assets/img/wasp-logo.png'; ?>" width="300" height="105" alt="Wasp Logo" />
 	</div>
-<!--
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#PEEK">
-			<span class="mat-item-single">PEEK</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#Inconel625">
-			<span class="mat-item-first">Inconel</span>
-			<span class="mat-item-second">625</span>
-		</a>
-	</div> 	
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#ABS">
-			<span class="mat-item-single">ABS</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#TPA">
-			<span class="mat-item-single">TPA</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#Nylon">
-			<span class="mat-item-single">Nylon</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#TPU">
-			<span class="mat-item-single">TPU</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#ULTEM1010">
-			<span class="mat-item-first">ULTEM</span>
-			<span class="mat-item-second">1010</span>
-		</a>
-	</div>
-	<div class="mat-slide dddCarouselItem">
-		<a href="materials.php#RESIN">
-			<span class="mat-item-single">RESIN</span>
-		</a>
-	</div> 
--->
+	-->
 </div>
-<!-- <div class="dddCarouselNavi">
-	<a class="dddCarouselPrev dddCarouselControl dddCarouselDisabled" data-target="#partners-home">Indietro</a>
-	<a class="dddCarouselNext dddCarouselControl" data-target="#partners-home">Avanti</a>
-</div> -->
