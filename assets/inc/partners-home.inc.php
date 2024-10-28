34c2d105a55ec1b5278793a9686f13ab1d26219c
imeuro <mauro.fioravanzi@gmail.com>
2024-10-28 15:25:27 +0100

    tolto logo wasp

--- assets/inc/partners-home.inc.php
+++ assets/inc/partners-home.inc.php
@@ -2,8 +2,9 @@
 	<div class="partner-slide">
 		<img src="<?php echo $basepath .'/assets/img/formlabs-logo.png'; ?>" width="300" height="105" alt="Formlabs Logo" />
 	</div>	
+	<!--
 	<div class="partner-slide">
-		<img src="<?php echo $basepath .'/assets/img/wasp-logo.png'; ?>" width="300" height="105" alt="Wasp Logo" />
+		<img src="<?php // echo $basepath .'/assets/img/wasp-logo.png'; ?>" width="300" height="105" alt="Wasp Logo" />
 	</div>
 <!--
 	<div class="mat-slide dddCarouselItem">
